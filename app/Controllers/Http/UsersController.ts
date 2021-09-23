47c60d89c2d35e4431d7a0462f6e1a6fb0966695
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-09-22 23:05:36 -0300

    feat: add create route

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,16 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
 
     public async create({ request }: HttpContextContract) {
-        const data = request.only(['name', 'username']);
-        console.log(data);
+        const { username, name } = request.only(['name', 'username']);
+
+        const user = await User.create({
+            username, name
+        });
+
+        return user;
     }
 
 }
